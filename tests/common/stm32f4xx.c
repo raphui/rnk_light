327efa2844f0ab3e112c77607ea5f44f5b1f79af
Raphael Poggi <poggi.raph@gmail.com>
2018-11-27 14:50:38 +0000

    tests: common: stm32f4xx: rework set sysclk

--- tests/common/stm32f4xx.c
+++ tests/common/stm32f4xx.c
@@ -6,82 +6,48 @@
 #include <armv7m/mpu.h>
 
 #define PLL_M		8
-#define PLL_Q		7
-#define PLL_N		336
+#define PLL_Q		4
+#define PLL_N		84
 #define PLL_P		2
-#define HSE_STARTUP_TIMEOUT    0x05000
-#define RESET		0
 
 void set_sys_clock(void)
 {
 	/* *****************************************************************************/
 	/*             PLL (clocked by HSE) used as System clock source                */
 	/* *****************************************************************************/
-	unsigned int StartUpCounter = 0, HSEStatus = 0;
+	/*  Select regulator voltage output Scale 1 mode */
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+	PWR->CR |= PWR_CR_VOS;
 
-	/*  Enable HSE */
-	RCC->CR |= ((unsigned int)RCC_CR_HSEON);
+	/*  HCLK = SYSCLK / 1*/
+	RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
 
-	/*  Wait till HSE is ready and if Time out is reached exit */
-	do
-	{
-		HSEStatus = RCC->CR & RCC_CR_HSERDY;
-		StartUpCounter++;
-	} while((HSEStatus == 0) && (StartUpCounter != HSE_STARTUP_TIMEOUT));
+	/*  PCLK2 = HCLK / 1*/
+	RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;
 
-	if ((RCC->CR & RCC_CR_HSERDY) != RESET)
-	{
-		HSEStatus = (unsigned int)0x01;
-	}
-	else
-	{
-		HSEStatus = (unsigned int)0x00;
-	}
+	/*  PCLK1 = HCLK / 2*/
+	RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
 
-	if (HSEStatus == (unsigned int)0x01)
-	{
-		/*  Select regulator voltage output Scale 1 mode */
-		RCC->APB1ENR |= RCC_APB1ENR_PWREN;
-		PWR->CR |= PWR_CR_VOS;
+	/*  Configure the main PLL */
+	RCC->PLLCFGR = PLL_M | (PLL_N << 6) | (((PLL_P >> 1) -1) << 16) | (PLL_Q << 24);
 
-		/*  HCLK = SYSCLK / 1*/
-		RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
+	/*  Enable the main PLL */
+	RCC->CR |= RCC_CR_PLLON;
 
-		/*  PCLK2 = HCLK / 1*/
-		RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;
-
-		/*  PCLK1 = HCLK / 2*/
-		RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
-
-		/*  Configure the main PLL */
-		RCC->PLLCFGR = PLL_M | (PLL_N << 6) | (((PLL_P >> 1) -1) << 16) |
-			(RCC_PLLCFGR_PLLSRC_HSE) | (PLL_Q << 24);
-
-		/*  Enable the main PLL */
-		RCC->CR |= RCC_CR_PLLON;
-
-		/*  Wait till the main PLL is ready */
-		while((RCC->CR & RCC_CR_PLLRDY) == 0)
-			;
-
-		/*  Configure Flash prefetch, Instruction cache, Data cache and wait state */
-		FLASH->ACR = FLASH_ACR_PRFTEN | FLASH_ACR_ICEN |FLASH_ACR_DCEN |FLASH_ACR_LATENCY_2WS;
-
-
-		/*  Select the main PLL as system clock source */
-		RCC->CFGR &= (unsigned int)((unsigned int)~(RCC_CFGR_SW));
-		RCC->CFGR |= RCC_CFGR_SW_PLL;
-
-		/*  Wait till the main PLL is used as system clock source */
-		while ((RCC->CFGR & (unsigned int)RCC_CFGR_SWS ) != RCC_CFGR_SWS_PLL);
+	/*  Wait till the main PLL is ready */
+	while((RCC->CR & RCC_CR_PLLRDY) == 0)
 		;
-	}
-	else
-	{ /*  If HSE fails to start-up, the application will have wrong clock
-	      configuration. User can add here some code to deal with this error */
-		while(1)
-			;
-	}
+
+	/*  Configure Flash prefetch, Instruction cache, Data cache and wait state */
+	FLASH->ACR = FLASH_ACR_PRFTEN | FLASH_ACR_ICEN |FLASH_ACR_DCEN |FLASH_ACR_LATENCY_2WS;
+
+	/*  Select the main PLL as system clock source */
+	RCC->CFGR &= (unsigned int)((unsigned int)~(RCC_CFGR_SW));
+	RCC->CFGR |= RCC_CFGR_SW_PLL;
+
+	/*  Wait till the main PLL is used as system clock source */
+	while ((RCC->CFGR & (unsigned int)RCC_CFGR_SWS ) != RCC_CFGR_SWS_PLL);
+		;
 }
 
 void low_level_init(void)
